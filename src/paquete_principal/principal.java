729c55632ed9d4481e7bc8d3194f95167b558669
Edgarbarco <ebarcoc@miumg.edu.gt>
2023-04-25 23:40:49 -0600

    Cambios que realizò Edgar Barco

--- src/paquete_principal/principal.java
+++ src/paquete_principal/principal.java
@@ -3,6 +3,8 @@ package paquete_principal;
 import java.util.*;
 public class principal {
     public static void main(String[] args) {
-        System.out.println(" Esta es una prueba");
+        //Identicando nuestros nombres
+        System.out.println("Edgar Barco");
+        System.out.println("Nelson Enriquez");
     }
 }
